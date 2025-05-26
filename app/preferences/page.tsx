aeb2cffda674b6bd6a94422333498fc22b310b48
Tobin South <tobin.south@gmail.com>
2025-05-25 22:09:04 -0700

    Adding auth to page

--- app/preferences/page.tsx
+++ app/preferences/page.tsx
@@ -2,6 +2,6 @@ import { withAuth } from "@workos-inc/authkit-nextjs";
 import { PreferencesForm } from "@/components/preferences/preferences-form";
 
 export default async function PreferencesPage() {
-  // await withAuth(); // Temporarily disabled for debugging
+  await withAuth(); // Temporarily disabled for debugging
   return <PreferencesForm />;
 } 
