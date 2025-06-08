dd26e9e917d1c91610e73262a41a053fc4cd2269
Tobin South <tobin.south@gmail.com>
2025-06-08 14:18:19 -0700

    Adding better middleware

--- middleware.ts
+++ middleware.ts
@@ -9,13 +9,10 @@ export default authkitMiddleware({
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+    // Protect all pages except:
+    // - API routes (/api/*)
+    // - the root page (/)
+    // - static files, images, favicon, public
+    '/((?!_next/static|_next/image|favicon.ico|public/|$).*)',
   ],
 };
