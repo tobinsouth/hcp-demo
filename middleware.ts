7c283048fd1387a64c30cfe692dd8e79175ddf57
Tobin South <tobin.south@gmail.com>
2025-06-10 10:44:17 -0700

    Simplify middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,6 @@
 import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
 
 export default authkitMiddleware({
-  middlewareAuth: {
-    enabled: true,
-    unauthenticatedPaths: ['/'],
-  },
   redirectUri:
     process.env.VERCEL_TARGET_ENV === "preview"
       ? `https://${process.env.VERCEL_URL}/callback`
@@ -12,12 +8,5 @@ export default authkitMiddleware({
 });
 
 export const config = {
-  matcher: [
-    // Protect all pages except:
-    // - API routes (/api/*)
-    // - the root page (/)
-    // - static files, images, favicon, public
-    '/((?!_next/static|_next/image|favicon.ico|public/|login|$).*)',
-    
-  ],
+  matcher: ["/", "/hcp"],
 };
